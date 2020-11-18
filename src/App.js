9639f6706a570f9969219aaf6bb97c1493fc90c3
GnanaPradeepRS <gnana.gundluru@riversand.com>
2020-11-19 02:06:39 +0530

    Added Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React , { Component }from 'react';
 import Main from './components/MainComponent';
-
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 class App extends Component {
 
   render() {
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,71 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-
-    renderDish(dish) {
-        if (dish != null)
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
-
-    renderComments() {
-        let options = { year: 'numeric', month: 'short', day: 'numeric' };
-        if (this.props.selectedDish != null) {
-            let comments = this.props.selectedDish.comments;
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className=" list-group list-group-flush">
-                        {
-                            comments.map((comment, index) => {
-                                return (
-                                    //unique key to handle key in list error in console
-                                    <li key={this.props.selectedDish.id + index} className="list-group-item">
-                                        {comment.comment}
-                                        <br />
-                                        -- {comment.author} ,  {new Date(comment.date).toLocaleDateString("en-US", options)}
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-            )
-        }
-        else {
-            return (
-                <div></div>
-            )
-        }
-
-    }
-
-    render() {
-
+const RenderDish = ({ dish }) => {
+    if (dish != null)
         return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    else
+        return (
+            <div></div>
+        );
+}
 
+const RenderComments = ({comments}) => {
+    let options = { year: 'numeric', month: 'short', day: 'numeric' };
+    if (comments != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className=" list-group list-group-flush">
+                    {
+                        comments.map((comment, index) => {
+                            return (
+                                //unique key to handle key in list error in console
+                                <li key={index} className="list-group-item">
+                                    {comment.comment}
+                                    <br />
+                                        -- {comment.author} ,  {new Date(comment.date).toLocaleDateString("en-US", options)}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const DishDetail = (props) => {
+    
+    if (props.selectedDish !== undefined) {
+        return (
             <div className="container">
                 <div className="row">
-                    {this.renderDish(this.props.selectedDish)}
-                    {this.renderComments()}
+                    <RenderDish dish={props.selectedDish} />
+                    <RenderComments comments={props.selectedDish.comments} />
                 </div>
             </div>
-
+        )
+    }
+    else {
+        return(
+            <div></div>
         )
     }
 }
+
 export default DishDetail;
