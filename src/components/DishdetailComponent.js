3833fe0875065fa436882d0624db3a01312dd04e
Pradeep <pradeepgundluru077@gmail.com>
2021-01-25 02:56:42 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { Errors, Control, LocalForm } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const minLength = (len) => (val) => !(val) || (val.length >= len)
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -104,7 +105,11 @@ class CommentForm extends Component {
 const RenderDish = ({dish}) => {
     if (dish != null)
         return (
-
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             <Card>
                 <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
@@ -112,6 +117,7 @@ const RenderDish = ({dish}) => {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
     else
         return (
@@ -127,16 +133,18 @@ const RenderComments = ({ comments, postComment, dishId }) => {
                 <h4>Comments</h4>
                 <ul className=" list-group list-group-flush">
                     {
-                        comments.map((comment, index) => {
-                            return (
-                                //unique key to handle key in list error in console
-                                <li key={index} className="list-group-item">
-                                    {comment.comment}
-                                    <br />
-                                        -- {comment.author} ,  {new Date(comment.date).toLocaleDateString("en-US", options)}
-                                </li>
-                            )
-                        })
+                    <Stagger in>
+                    {comments.map((comment) => {
+                        return (
+                            <Fade in>
+                            <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                            </Fade>
+                        );
+                    })}
+                    </Stagger>
                     }
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,51 +1,58 @@
 import React from 'react';
-import {Card , CardBody , CardTitle , CardText ,CardImg , CardSubtitle} from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, CardImg, CardSubtitle } from 'reactstrap';
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
-const RenderCard = ({item , isLoading , errMess}) => {
+const RenderCard = ({ item, isLoading, errMess }) => {
   if (isLoading) {
-    return(
+    return (
       <Loading />
     )
   }
-  else if(errMess){
-    return(
+  else if (errMess) {
+    return (
       <h4>
         {errMess}
       </h4>
     )
   }
-  else if(item){
-    return(
-      <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {item.designation ? <CardSubtitle> {item.designation} </CardSubtitle> : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
+  else if (item) {
+    return (
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle> {item.designation} </CardSubtitle> : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     )
   }
 }
 
 function Home(props) {
-    return(
-      <div className="container">
+  return (
+    <div className="container">
       <div className="row align-items-start">
-          <div className="col-12 col-md m-1">
-              <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishErrMess} />
-          </div>
-          <div className="col-12 col-md m-1">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishErrMess} />
+        </div>
+        <div className="col-12 col-md m-1">
           <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
-          </div>
-          <div className="col-12 col-md m-1">
-              <RenderCard item={props.leader} />
-          </div>
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
       </div>
-  </div>
-    );
+    </div>
+  );
 }
 
 export default Home;   
